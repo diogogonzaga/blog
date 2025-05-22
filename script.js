e951f30d462c49348f6e0cdbf9d82bef4b45da87
DiogogongasDev <diogodgonzaga@gmail.com>
2025-05-22 11:30:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,22 @@
 const anoNascimento = 2007; // Altere aqui
 const anoAtual = new Date().getFullYear();
 document.getElementById('idade').textContent = anoAtual - anoNascimento;
+
+
+function alerta(event, element) {
+  event.preventDefault(); // Impede o redirecionamento imediato
+
+  Swal.fire({
+    title: "Esta página ainda não está formatada para dispositivos móveis!",
+    text: "Queres mesmo abrir esta página?",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonText: "Sim, continuar",
+    cancelButtonText: "Cancelar"
+  }).then((result) => {
+    if (result.isConfirmed) {
+      // Só redireciona após confirmação
+      window.open(element.href, '_blank');
+    }
+  });
+}
